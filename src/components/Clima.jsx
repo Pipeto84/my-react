9a27902576e29a59091f1ec20b95bde4e4921306
Pipeto84 <pipejarauribeboteroa@gmail.com>
2024-07-12 15:25:05 -0500

    Temperatura

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -3,10 +3,14 @@ import { ClimaContext } from '../context/ClimaContext'
 
 export const Clima = () => {
   const [datos, setDatos] = useState(null)
+  const Kelvin = 273.15
 
   const {infoCity} = useContext(ClimaContext)
   console.log(infoCity.name)
   return (
-    <div>{infoCity.name}</div>
+    <div>
+      <h2>{infoCity.name}, {infoCity.sys.country}</h2>
+      <p>Temperatura: {(infoCity.main.temp - Kelvin).toFixed(2)} ॰C</p>
+    </div>
   )
 }
