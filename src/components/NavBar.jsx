67afe76d65ccbe4a74643c3c9094386f60f9de6e
Pipeto84 <pipejarauribeboteroa@gmail.com>
2024-07-25 11:27:17 -0500

    color navbar & badge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import  ShoppingCartIcon  from '@mui/icons-material/ShoppingCart'
 import { Badge } from '@mui/material'
+import { grey,red } from '@mui/material/colors';
 import { NavLink } from 'react-router-dom'
 import {useContext} from 'react'
 import { CarritoContext } from '../context/CarritoContext'
 
 export const NavBar = () => {
   const {listaCompras} = useContext(CarritoContext)
+  const colorBadge = red[400]
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
         <NavLink to='/buscador' className="navbar-brand" >React Pipeto</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +30,7 @@ export const NavBar = () => {
         </div>
         <NavLink to='/carrito'>
           <Badge badgeContent={listaCompras.length} color='secondary'>
-            <ShoppingCartIcon color='action' />
+            <ShoppingCartIcon color='danger' />
           </Badge>
         </NavLink>
       </div>
