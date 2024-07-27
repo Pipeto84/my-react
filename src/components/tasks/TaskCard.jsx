3a38351bd6ba2578b14dad6b8a47ad6ab35815df
Pipeto84 <pipejarauribeboteroa@gmail.com>
2024-07-27 16:40:34 -0500

    tareas alineadas

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react"
 import { TareasContext } from "../../context/TareasContext"
-
+import "../../styles/Tasks.css";
 
 export const TaskCard = ({task}) => {
   const {eliminarTarea} = useContext(TareasContext)
   return (
-    <div className="contenedorTareas">
-      <h3 className="titulo">{task.titulo}</h3>
-      <p className="descripcion">{task.descripcion}</p>
+    <div className="contenedorTarea">
+      <h3 className="tituloTarea">{task.titulo}</h3>
+      <p className="descripcionTarea">{task.descripcion}</p>
       <button 
         className="btn btn-danger botonEliminarTarea"
         onClick={()=>eliminarTarea(task.id)}
--- src/components/tasks/TaskList.jsx
+++ src/components/tasks/TaskList.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { TareasContext } from "../../context/TareasContext"
 import { TaskCard } from "./TaskCard"
+import "../../styles/Tasks.css";
 
 export const TaskList = () => {
   const {tasks} = useContext(TareasContext)
